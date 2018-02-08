e51e83c2f042351aebe285a81f0bb4c5981735a4
Oshima Tlholoe <ptlholoe@fnb.co.za>
2018-02-08 08:00:05 +0200

    Add basic user input practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  message: String;
+  values: String = '';
+  text_value: String = '';
   title = 'app';
+
+  onClickMe() {
+    this.message = 'You have clicked Me!!!';
+  }
+
+  onKey(event: any) {
+    this.values  += event.target.value + '|';
+  }
+
+  onEnter(value: String) {
+    this.text_value = value;
+  }
 }
